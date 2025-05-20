2de29ad10b25e229b31f42f04b78109d6e986e66
Rajasenan D <163999412+RajasenanD@users.noreply.github.com>
2025-05-20 21:51:03 +0530

    Update simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -31,3 +31,16 @@ def calc(a,b):
 
 result = calc(a,b)
 print(result)
+
+
+'''Simple calculator
+
+Uses if condition inside while loop to check whether the input is within add,sub,mul,div otherwise it prints 'Enter mentioned operation' and continue to ask again until required operation in entered.
+
+Uses try and except inside while loop to check Valueerror whether the input in integer otherwise prints 'Enter number as input' otherwise continue to ask for input till number is given.
+
+Uses function calc() to do operations mentioned in the input() using if conditions and returns the value of operations.
+
+Call the function calc() and assign the value in a variable result.
+
+Then prints result'''
