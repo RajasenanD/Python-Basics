63778188f590a5706fe0f48974e4ad9e002c76ea
Rajasenan D <163999412+RajasenanD@users.noreply.github.com>
2025-05-20 21:51:48 +0530

    Update simple_calculator_1.py

--- simple_calculator_1.py
+++ simple_calculator_1.py
@@ -16,3 +16,9 @@ try:
             print(f'The result is {a/b}')
 except Exception as e:
     print('Enter a valid value for a and b...')
+
+
+'''simple_calculator_1
+Used try and except to determine the input as integer if not shows 'Enter a valid value;
+Used o as variable which take input of operation to perform.
+Used match and case to made the operation such as +,-,*,/ and print(the result is {operation})'''
